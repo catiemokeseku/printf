addda252d55c0bd7496c890612e32066d8b1f337
catiemokeseku <43410683+catiemokeseku@users.noreply.github.com>
2022-10-19 03:57:16 +0000

    printf

--- _printf.c
+++ _printf.c
@@ -12,29 +12,42 @@
 
 int _printf(const char *format, ...)
 {
-    int i = 0;
-    int n = 0;
-    va_list a;
+	va_list args;
+	int i = 0, chars_printed = 0;
 
-    va_start(a, format);
-
-    while (format && format[i])
-    {
-        if (format[i] != '%')
-        {
-            n += _putchar(format[i]);
-        }
-        else if (format[i+1])
-        {
-            i++;
-            if (format[i] == 'c')
-            {
-                n += _putchar(va_arg(a, int));
-            }
-            else if (format[i] == 's')
-            {
-                n += print_string(va_arg(a, char *));
-            }
-        }
-    }
+	va_start(args, format);
+	while (format && format[i])
+	{
+		if (format[i] != '%')
+		{
+			chars_printed += _putchar(format[i]);
+		}
+		else if (format[i + 1])
+		{
+			i++;
+			if (format[i] == 'c')
+				chars_printed += _putchar(va_arg(args, int));
+			else if (format[i] == 's')
+				chars_printed += print_string(va_arg(args, char *));
+			else if (format[i] == 'd' || format[i] == 'i')
+				chars_printed += print_num(va_arg(args, int));
+			else if (format[i] == 'b')
+				chars_printed += print_binary((unsigned int)va_arg(args, int));
+			else if (format[i] == 'r')
+				chars_printed += print_reverse(va_arg(args, char *));
+			else if (format[i] == 'R')
+				chars_printed += print_rot13(va_arg(args, char *));
+			else if (format[i] == 'o' || format[i] == 'u' ||
+			format[i] == 'x' || format[i] == 'X')
+				chars_printed += print_odh(format[i], (unsigned int)va_arg(args, int));
+			else if (format[i] == 'S')
+				chars_printed += print_S(va_arg(args, char *));
+			else if (format[i] == 'p')
+				chars_printed += print_pointer(va_arg(args, void *));
+			else
+				chars_printed += print_unknown_spec(format[i]);
+		}
+		i++;
+	}
+	return (chars_printed);
 }
